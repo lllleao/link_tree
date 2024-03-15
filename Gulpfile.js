287d5e40aabdf9e59d8140ecfeb5ee8fe044f7a5
leao <lucasleaolima@gmail.com>
2024-03-15 10:34:02 -0300

    Configurando build

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const less = require('gulp-less')
 const sourceMaps = require('gulp-sourcemaps')
+const imagemin = require('gulp-imagemin')
 
 function styles() {
     return gulp.src('./source/styles/main.less')
@@ -10,7 +11,14 @@ function styles() {
     .pipe(gulp.dest('./build/styles'))
 }
 
-exports.watch = function() {
-    gulp.watch('./source/styles/*.less', gulp.series(styles))
+function images() {
+    return gulp.src('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'))
 }
 
+exports.default = gulp.series(styles, images)
+
+exports.watch = function() {
+    gulp.watch('./source/styles/*.less', gulp.series(styles, images))
+}
